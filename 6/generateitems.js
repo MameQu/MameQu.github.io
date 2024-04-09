5c4a7510954f2dbf324bcf4c27b53c3884dad63d
MameQu <140203350+MameQu@users.noreply.github.com>
2024-04-09 15:57:00 +0900

    _required

---
+++ 6/generateitems.js
@@ -0,0 +1,44 @@
+var sample = {
+    "Acore": [ {
+        "Name": "Attak1",
+        "Level": "1",
+    },{
+        "Name": "Attak2",
+        "Level": "2",
+    },{
+        "Name": "Attak3",
+        "Level": "3",
+    },{
+        "Name": "Attak4",
+        "Level": "4",
+    },
+    ],
+    "SCore": [ {
+        "Name": "Skill1",
+        "Level": "1",
+    },{
+        "Name": "Skill2",
+        "Level": "2",
+    },{
+        "Name": "Skill3",
+        "Level": "3",
+    },
+    ],
+    "UCore": [ {
+        "Name": "Ultimate1",
+        "Level": "1",
+    },{
+        "Name": "Skill1",
+        "Level": "2",
+    },{
+        "Name": "Skill1",
+        "Level": "2",
+    },{
+        "Name": "Skill1",
+        "Level": "2",
+    },{
+        "Name": "Skill1",
+        "Level": "2",
+    },
+    ],
+}
