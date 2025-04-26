6999f7e65a1cf4ae3cdfb41c983abd01408b42c2
MameQu <140203350+MameQu@users.noreply.github.com>
2025-04-26 20:12:42 +0900

    Update contents.js

--- 7/contents.js
+++ 7/contents.js
@@ -35,12 +35,13 @@ function overlayContents() {
     const input = addDiv(chat, "input");
     const link = addTextarea(input, "link");
     const submity = addButton(input, "submity");
+    if(data.e) {
+        s = data.e;
+        input.innerHTML = `<iframe src='https://www.youtube.com/live_chat?v=${s}&embed_domain=mamequ.github.io' style='height:100%;width:100%'></iframe>`;
+    };
     submity.onclick = function() {
         let s = link.value.match(/[?&]v=([^&]+)/)[1];
         store('e',s);
-        if(data.e) {
-            s = data.e;
-        }
         input.innerHTML = `<iframe src='https://www.youtube.com/live_chat?v=${s}&embed_domain=mamequ.github.io' style='height:100%;width:100%'></iframe>`;
     }
 
