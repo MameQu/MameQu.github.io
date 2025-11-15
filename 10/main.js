39c76c57b3519e76559ea46168d86882e6748e86
MameQu <140203350+MameQu@users.noreply.github.com>
2025-11-15 21:50:28 +0900

    Update main.js

--- 10/main.js
+++ 10/main.js
@@ -1,18 +1,30 @@
-const cubism4Model =
-    "model/gag0.model3.json";
+const app = new PIXI.Application({
+    view: document.getElementById("canvas"),
+    autoStart: true,
+    background: 0x202020,
+    resizeTo: window
+});
 
-(async function main() {
-    const app = new PIXI.Application({
-        view: document.getElementById("canvas"),
-        autoStart: true,
-        resizeTo: window
+PIXI.live2d.Live2DModel.from("model/gag0.model3.json").then(model => {
+    app.stage.addChild(model);
+
+    model.scale.set(0.5);
+    model.position.set(window.innerWidth / 2, window.innerHeight / 2);
+
+    let start = performance.now();
+
+    app.ticker.add(() => {
+        const t = (performance.now() - start) / 1000;
+
+        const breath = Math.sin(t * 2.0);
+        const blink = Math.max(0, Math.sin(t * 4.0));
+        const hair = Math.sin(t * 3.5) * 0.2;
+
+        const core = model.internalModel.coreModel;
+        core.setParameterValueById("ParamBreath", breath);
+        core.setParameterValueById("blink", blink);
+        core.setParameterValueById("hair", hair);
+
+        model.internalModel.update();
     });
-
-    const model4 = await PIXI.live2d.Live2DModel.from(cubism4Model);
-
-    app.stage.addChild(model4);
-
-    model4.scale.set(0.25);
-
-    model4.x = 300;
-})();
+});
